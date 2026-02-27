2c9d59e1f345215eef915fa64dc4aba161ad6ab7
Alexandre Teixeira <mrtalexandre@gmail.com>
2026-02-27 00:36:38 -0300

    feat: add CleanExportsJob to delete old export files
    - Create CleanExportsJob that deletes exports older than 30 days
    - Schedule in recurring.yml to run every Sunday at 3am
    - Prevents disk space leaks from accumulating CSV/JSON/MD reports

---
+++ app/jobs/clean_exports_job.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class CleanExportsJob < ApplicationJob
+  queue_as :default
+
+  # Delete export files older than 30 days to prevent disk space leaks.
+  def perform(max_age_days: 30)
+    exports_dir = Rails.root.join("exports")
+    return unless exports_dir.exist?
+
+    cutoff = max_age_days.days.ago
+    deleted = 0
+
+    Dir.glob(exports_dir.join("*")).each do |file|
+      next unless File.file?(file)
+      next unless File.mtime(file) < cutoff
+
+      File.delete(file)
+      deleted += 1
+    end
+
+    Rails.logger.info("CleanExportsJob: deleted #{deleted} export files older than #{max_age_days} days")
+  end
+end
